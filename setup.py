edff374a995029b3d6904f53672100a1e05d903e
Reto Stamm <reto@retostamm.com>
2023-02-10 11:49:45 -0800

    Added details to setup.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='phronesitron',
+   version='0.0.1',
+   description='Commandline tool to request help from ML systems',
+   author='Reto Stamm',
+   author_email='phronesitron@retostamm.com',
+   packages=['phronesitron'],  
+   install_requires=['wheel', 'openai', 'textwrap', 'argparse', 'datetime', 'pyperlcip'], 
+   scripts=['phronesitron/ph']
+)
+
+import openai
+import os
+import sys
+import textwrap as tr
+import argparse
+import time
+from termcolor import colored
+import datetime
+import pyperclip as pc
