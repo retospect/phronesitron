70e4af20e0c337947b2ffeaa2bcf60a1db37d375
Reto Stamm <reto@retostamm.com>
2023-02-10 12:32:06 -0800

    Rev release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='phronesitron',
-   version='0.0.4',
+   version='0.0.5',
    description='Commandline tool to request help from ML systems',
    author='Reto Stamm',
    author_email='phronesitron@retostamm.com',
