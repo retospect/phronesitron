075836f3d2412c978a27c800d208544e43f50c93
Reto Stamm <reto@retostamm.com>
2023-02-11 08:53:39 -0800

    trying to change default

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,6 +3,8 @@ import sys
 
 
 def test_flit_help():
-    p = Popen([sys.executable, "-m", "ph", "--help"], stdout=PIPE, stderr=STDOUT)
+    p = Popen(
+        [sys.executable, "-m", "phronesitron", "--help"], stdout=PIPE, stderr=STDOUT
+    )
     out, _ = p.communicate()
     assert "very random" in out.decode("utf-8", "replace")
