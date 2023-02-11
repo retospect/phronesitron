8e04bc2538b78b40af9d5f9b44c3a2e683f8aa1b
Reto Stamm <reto@retostamm.com>
2023-02-11 08:57:49 -0800

    Testing two targets

--- tests/test_command.py
+++ tests/test_command.py
@@ -2,9 +2,12 @@ from subprocess import Popen, PIPE, STDOUT
 import sys
 
 
-def test_flit_help():
-    p = Popen(
-        [sys.executable, "-m", "ph", "--help"], stdout=PIPE, stderr=STDOUT
-    )
+def test_ph_help():
+    p = Popen([sys.executable, "-m", "ph", "--help"], stdout=PIPE, stderr=STDOUT)
+    out, _ = p.communicate()
+    assert "very random" in out.decode("utf-8", "replace")
+
+def test_phronesitron_help():
+    p = Popen([sys.executable, "-m", "phronesitron", "--help"], stdout=PIPE, stderr=STDOUT)
     out, _ = p.communicate()
     assert "very random" in out.decode("utf-8", "replace")
