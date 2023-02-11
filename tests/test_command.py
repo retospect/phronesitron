5e17706e6e6851b1f938e4aef1308996303b727a
Reto Stamm <reto@retostamm.com>
2023-02-11 08:42:22 -0800

    Fixed formatting

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,7 +1,8 @@
 from subprocess import Popen, PIPE, STDOUT
 import sys
 
+
 def test_flit_help():
-    p = Popen([sys.executable, '-m', 'ph', '--help'], stdout=PIPE, stderr=STDOUT)
+    p = Popen([sys.executable, "-m", "ph", "--help"], stdout=PIPE, stderr=STDOUT)
     out, _ = p.communicate()
-    assert 'very random' in out.decode('utf-8', 'replace')
+    assert "very random" in out.decode("utf-8", "replace")
