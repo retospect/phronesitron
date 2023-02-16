52fa49241f0f39988af50e03f2abf775398a80df
Reto Stamm <reto@retostamm.com>
2023-02-16 12:59:52 -0800

    Adding lock file because it takes 2 minutes to generate

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,20 +1,10 @@
 from subprocess import Popen, PIPE, STDOUT
 import sys
 
-
-def test_ph_help():
-    pass
-    return
-    p = Popen([sys.executable, "-m", "ph", "--help"], stdout=PIPE, stderr=STDOUT)
-    out, _ = p.communicate()
-    assert "very random" in out.decode("utf-8", "replace")
-
-
 def test_phronesitron_help():
-    pass
-    return
+    ''' Checks that the commandline install works '''
     p = Popen(
-        [sys.executable, "-m", "phronesitron", "--help"], stdout=PIPE, stderr=STDOUT
+        [sys.executable, "ph"], stdout=PIPE, stderr=STDOUT
     )
     out, _ = p.communicate()
     assert "very random" in out.decode("utf-8", "replace")
